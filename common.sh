1f2000119dcc8b7998268467782b9bc64bcd4e2b
Nicolas Trangosi <nicolas.trangosi@dcbrain.com>
2022-08-05 17:02:20 +0200

    use DCbrainOrg

--- common.sh
+++ common.sh
@@ -2,6 +2,7 @@
 
 # Common script used by all others to define variables and stay DRY
 DOCKER_CONTAINER='docker-kong-oidc'
-DOCKER_IMAGE="local/$DOCKER_CONTAINER:2.8.1-1"
+DOCKER_IMAGE="docker.dcbra.in/dcbrain/$DOCKER_CONTAINER:2.8.1-2-dcbrain"
 KONG_LOCAL_HTTP_PORT=${KONG_LOCAL_HTTP_PORT:-180}
 KONG_LOCAL_HTTPS_PORT=${KONG_LOCAL_HTTPS_PORT:-143}
+#
